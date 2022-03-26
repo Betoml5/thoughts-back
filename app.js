1df81cdaec30c78ca90a3bd5d54b769e57f343a5
Betoml5 <betoml5@hotmail.com>
2022-03-26 16:31:48 -0600

    Add env vars

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 
+async function connectDB() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${config.dbUser}:${config.dbPassword}@pagos-tec.mih9c.mongodb.net/pagos-tec-db?retryWrites=true&w=majority`
+    );
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-app.get("/", (req, res) => {
-    res.send({ message: "Hola bris" })
-})
+connectDB();
 
 app.listen(2424, () => {
-    console.log("server listenting at port 2424")
-})
+  console.log("server listenting at port 2424");
+});
 
 module.exports = app;
-
-
